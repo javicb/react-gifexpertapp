308b4a5af99712a1ce32f45093305ba452d68c32
Javier Cruz <javier.cruz.bonoso@avanade.com>
2020-07-17 20:03:43 +0200

    communication between components

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -17,7 +17,7 @@ export const GifExpertApp = () => {
    return (
       <>
          <h2>GifExpertApp</h2>
-         <AddCategory></AddCategory>
+         <AddCategory setCategories={setCategories} />
          <hr></hr>
          <ol>
             {
--- src/components/AddCategory/AddCategory.js
+++ src/components/AddCategory/AddCategory.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
+import PropTypes from 'prop-types';
 
-export const AddCategory = () => {
-   const initialState = 'Hola Mundo';
-   const [inputValue, setInputValue] = useState(initialState)
+export const AddCategory = ({ setCategories }) => {
+
+   const initialValue = '';
+   const [inputValue, setInputValue] = useState(initialValue);
 
    const handleInputChange = (e) => {
       setInputValue(e.target.value)
@@ -10,13 +12,37 @@ export const AddCategory = () => {
 
    const handleSubmit = (e) => {
       e.preventDefault();
+
+      if (inputValue.trim().length > 2) {
+         setCategories(cats => {
+
+            // Guardar las categorias que hay
+            const exist = cats.find(x => x === inputValue);
+
+            // Si existe, devuelte las categorías ya existentes,
+            // si no, añade la nueva categoría
+            if (exist) {
+               return [...cats];
+            } else {
+               return [inputValue, ...cats];
+            }
+         });
+
+         setInputValue('');
+      }
    }
 
    return (
       <>
          <form onSubmit={handleSubmit}>
-            <input type='text' value={inputValue} onChange={handleInputChange}></input>
+            <input type='text'
+               value={inputValue}
+               onChange={handleInputChange}></input>
          </form>
       </>
    )
 }
+
+AddCategory.propTypes = {
+   setCategories: PropTypes.func.isRequired
+};
