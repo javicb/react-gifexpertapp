7919276f726a49423f74b25e12c0895ed5ab6fa0
Javier Cruz <javier.cruz.bonoso@avanade.com>
2020-07-19 17:24:21 +0200

    use effect

--- src/components/GifGrid/GifGrid.js
+++ src/components/GifGrid/GifGrid.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export const GifGrid = ({ category }) => {
+
+   const [count, setCount] = useState(0)
+
+   useEffect(() => {
+      getGifs();
+   }, []);
 
    const getGifs = async () => {
       const url = 'https://api.giphy.com/v1/gifs/search?q=Rick and Morty&limit=10&api_key=6q16rM6usThsbu9X2kmpiFrTbcNq4gFN';
@@ -17,11 +23,13 @@ export const GifGrid = ({ category }) => {
       console.log(gifs);
    }
 
-   getGifs();
+   // getGifs();
 
    return (
       <>
          <h3>{category}</h3>
+         <h3>{count}</h3>
+         <button onClick={() => setCount(count + 1)}></button>
       </>
    )
 }
