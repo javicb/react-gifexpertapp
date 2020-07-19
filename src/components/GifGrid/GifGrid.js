3bafe789593a8209e6e082fa0edb7b06959e9646
Javier Cruz <javier.cruz.bonoso@avanade.com>
2020-07-19 18:18:51 +0200

    show image and title

--- src/components/GifGrid/GifGrid.js
+++ src/components/GifGrid/GifGrid.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react'
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
 
-   const [count, setCount] = useState(0)
+   const [images, setImages] = useState([]);
 
    useEffect(() => {
       getGifs();
@@ -21,15 +22,18 @@ export const GifGrid = ({ category }) => {
       })
 
       console.log(gifs);
+      setImages(gifs);
    }
-
-   // getGifs();
 
    return (
       <>
          <h3>{category}</h3>
-         <h3>{count}</h3>
-         <button onClick={() => setCount(count + 1)}></button>
+
+         {
+            images.map((img) => (
+               <GifGridItem key={img.id} {...img} />
+            ))
+         }
       </>
    )
 }
