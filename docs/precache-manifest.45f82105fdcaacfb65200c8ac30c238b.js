0871e2ca0a08da5465a96b3929d4052a17735ad9
Javier Cruz <javi.cb@gmail.com>
2020-07-19 19:37:30 +0200

    rename build -> docs for gitpages

---
+++ docs/precache-manifest.45f82105fdcaacfb65200c8ac30c238b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "173f0ce23c045a6f8912b3bd84ad9679",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c5469e4694017e2dbd7",
+    "url": "/static/css/main.6863ca2f.chunk.css"
+  },
+  {
+    "revision": "7df6532c0dcdd8b491e8",
+    "url": "/static/js/2.71d7883b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.71d7883b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c5469e4694017e2dbd7",
+    "url": "/static/js/main.55475edf.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
