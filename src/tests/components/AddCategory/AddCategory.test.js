5f54cd053e32469cac5c2e7080959204032d1184
Javier Cruz <javi.cb@gmail.com>
2020-07-20 19:27:51 +0200

    test simulate submit

--- src/tests/components/AddCategory/AddCategory.test.js
+++ src/tests/components/AddCategory/AddCategory.test.js
@@ -5,8 +5,13 @@ import { AddCategory } from '../../../components/AddCategory/AddCategory';
 
 describe('Pruebas con el AddCategory', () => {
 
-   const setCategories = () => { };
-   const wrapper = shallow(<AddCategory setCategories={setCategories} />);
+   const setCategories = jest.fn();
+   let wrapper = shallow(<AddCategory setCategories={setCategories} />); // si eliminamos esta asignación perdemos intellisense
+
+   beforeEach(() => {
+      jest.clearAllMocks();
+      wrapper = shallow(<AddCategory setCategories={setCategories} />);
+   })
 
    test('Debe mostrarse correctamente ', () => {
       expect(wrapper).toMatchSnapshot();
@@ -16,7 +21,21 @@ describe('Pruebas con el AddCategory', () => {
       const input = wrapper.find('input');
       const value = 'Hola mundo';
       input.simulate('change', { target: { value } });
-   })
+   });
+
+   test('No debe llamar la infomración con submit', () => {
+      wrapper.find('form').simulate('submit', { preventDefault: () => { } });
+      expect(setCategories).not.toHaveBeenCalled();
+   });
+
+   test('Debe llamar al setCategories y limpiar la caja de texto', () => {
+      const value = 'Hola mundo';
+      wrapper.find('input').simulate('change', { target: { value } });
+      wrapper.find('form').simulate('submit', { preventDefault() { } });
+
+      expect(setCategories).toHaveBeenCalled();
+      expect(wrapper.find('input').prop('value')).toBe('');
+   });
 
 
 })
